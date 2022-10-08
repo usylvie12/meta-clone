3d6728197c6d6944e5756ef66a5b4097b0991552
usylvie12 <uwasylvie12@outlook.com>
2022-10-08 11:39:56 +0200

    add responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['/*.html'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      sm:'480px',
+      sm:'768px',
+      sm:'976px',
+      sm:'1440px'
+    },
+      colors:{
+        'slate':'#1e293b',
+      }
+    },
+    extend: {
+      
+    }
+  }
+  
+
